d0f933c2d247f248e438b821aa9993c5f9f2b2eb
ruaruarua <shen.zek@northeastern.edu>
2023-03-20 18:04:38 -0400

    update cmake lists

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Northeastern/CS4150/Project3/Project2
+# Install script for directory: E:/Northeastern/github/Project2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/Northeastern/CS4150/Project3/Project2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/Northeastern/github/Project2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
